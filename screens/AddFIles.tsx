b74f1375f924b82419fd081c74233610fd03a3fb
jukialen <piteru1910@yahoo.com>
2022-03-30 16:59:38 +0200

    Update AddFiles || Adding storage rules

--- screens/AddFIles.tsx
+++ screens/AddFIles.tsx
@@ -1,10 +1,9 @@
-import { FC, useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, Button, TextInput, Image, Platform } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import * as ImagePicker from 'expo-image-picker';
 import { auth, storage } from '../firebase';
-import { getDownloadURL, ref, uploadBytesResumable, UploadTask } from 'firebase/storage';
-import { UploadTaskSnapshot } from '@firebase/storage';
+import { getDownloadURL, ref, uploadBytesResumable, UploadTask, UploadTaskSnapshot } from 'firebase/storage';
 import { addDoc, CollectionReference } from 'firebase/firestore';
 import * as Localization from 'expo-localization';
 import i18n from 'i18n-js';
@@ -15,13 +14,14 @@ import { Field, Form, Formik, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { SchemaValidation } from '../shemasValidation/schemaValidation';
 
-import { animationsCollectionRef, photosCollectionRef, videosCollectionRef } from 'references/referencesFirebase';
+import { animationsCollectionRef, photosCollectionRef, videosCollectionRef } from '../references/referencesFirebase';
 
 import { FormType } from 'types/global.types';
+import { globalColors } from '../styles/variables';
 
 
 // import { FormError } from '../../../../../components/molecules/FormError/FormError';
-// import { Alerts } from 'components/atoms/Alerts/Alerts';
+import { Alerts } from '../components/Atoms/Alerts/Alerts';
 // import { Progress } from '@chakra-ui/react';
 
 type FileDataType = {
@@ -36,7 +36,6 @@ export const AddFiles = () => {
   const [file, setFile] = useState<Blob | Uint8Array | ArrayBuffer | ImagePicker.ImageInfo | null>(null);
   const [valuesFields, setValuesFields] = useState<string>('');
   const [progressUpload, setProgressUpload] = useState<number>(0);
-  // const data = useHookSWR();
   const [tag, setTags] = useState<string>('');
 
   i18n.fallbacks = true;
@@ -63,34 +62,29 @@ export const AddFiles = () => {
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: .2,
-      exif: true
-    });
-
-    console.log('res',result);
-
+    const result = await ImagePicker.launchImageLibraryAsync()
+ 
     if (!result.cancelled) {
       setFile(result);
     }
-
   };
   
   const uploadFiles = async ({ tags }: FileDataType, { resetForm }: FormType) => {
-    // @ts-ignore
-    const photosRef = ref(storage, `${user?.uid}/photos/${file?.name}`);
-    // @ts-ignore
-    const videosRef = ref(storage, `${user?.uid}/videos/${file?.name}`);
-    // @ts-ignore
-    const animationsRef = ref(storage, `${user?.uid}/animations/${file?.name}`);
-      // @ts-ignore
+          // @ts-ignore
     const { uri } = file;
+      const split = uri.split('/')
+      const fileName = split[split.length - 1]
 
+    // @ts-ignore
+    const photosRef = ref(storage, `${user?.uid}/photos/${fileName}`);
+    // @ts-ignore
+    const videosRef = ref(storage, `${user?.uid}/videos/${fileName}`);
+    // @ts-ignore
+    const animationsRef = ref(storage, `${user?.uid}/animations/${fileName}`);
+
+      
     let upload: UploadTask;
-  
+
     switch (tags) {
       case `Animations`:
         upload = uploadBytesResumable(animationsRef, uri);
@@ -102,74 +96,53 @@ export const AddFiles = () => {
         upload = uploadBytesResumable(photosRef, uri);
     }
   
-    console.log(file)
-    console.log(tags)
     let refName: string;
 
-    // upload.on('state_changed', (snapshot: UploadTaskSnapshot) => {
-    //     const progress: number = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    //     setProgressUpload(progress);
+    upload.on('state_changed', (snapshot: UploadTaskSnapshot) => {
+        const progress: number = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setProgressUpload(progress);
       
-    //     switch (snapshot.state) {
-    //       case 'running':
-    //         setValuesFields('Upload is running');
-    //         return refName = snapshot.ref.name;
-    //       case 'paused':
-    //         setValuesFields('Upload is paused');
-    //         break;
-    //     }
-    //   }, (e: Error) => {
-    //     console.error(e);
-    //     setValuesFields(`${data?.AnotherForm?.notUploadFile`);
-    //   },
-    //   async () => {
-    //     const sendToFirestore = (colRef: CollectionReference, url: string) => {
-    //       addDoc(colRef, {
-    //         fileUrl: url,
-    //         description: refName,
-    //         tag: tags,
-    //         timeCreated: Date.now(),
-    //         uid: user?.uid
-    //       });
-    //       setValuesFields(`${data?.AnotherForm?.uploadFile}`);
-    //       setFile(null);
-    //       resetForm(initialValues);
-    //     };
+        switch (snapshot.state) {
+          case 'running':
+            setValuesFields('Upload is running');
+            return refName = snapshot.ref.name;
+          case 'paused':
+            setValuesFields('Upload is paused');
+            break;
+        }
+      }, (e: Error) => {
+        console.error(e);
+        setValuesFields(i18n.t('AnotherForm.notUploadFile'));
+      },
+      async () => {
+        const sendToFirestore = (colRef: CollectionReference, url: string) => {
+          addDoc(colRef, {
+            fileUrl: url,
+            description: refName,
+            tag: tags,
+            timeCreated: Date.now(),
+            uid: user?.uid
+          });
+          setValuesFields(i18n.t('AnotherForm.uploadFile'));
+          setFile(null);
+          resetForm(initialValues);
+        };
         
-    //     switch (tags) {
-    //       case `${data?.Aside?.animations}`:
-    //         const animationURL = await getDownloadURL(animationsRef);
-    //         sendToFirestore(animationsCollectionRef(), animationURL);
-    //         break;
-    //       case `${data?.Aside?.videos}`:
-    //         const videoURL = await getDownloadURL(videosRef);
-    //         sendToFirestore(videosCollectionRef(), videoURL);
-    //         break;
-    //       default:
-    //         const photoURL = await getDownloadURL(photosRef);
-    //         sendToFirestore(photosCollectionRef(), photoURL);
-    //     }
-    //   });
+        switch (tags) {
+          case i18n.t('Aside.animations'):
+            const animationURL = await getDownloadURL(animationsRef);
+            sendToFirestore(animationsCollectionRef(), animationURL);
+            break;
+          case i18n.t('Aside.videos'):
+            const videoURL = await getDownloadURL(videosRef);
+            sendToFirestore(videosCollectionRef(), videoURL);
+            break;
+          default:
+            const photoURL = await getDownloadURL(photosRef);
+            sendToFirestore(photosCollectionRef(), photoURL);
+        }
+      });
   };
-  
-  // const managedUpload = (state: string) => {
-  //   switch (state) {
-  //     case 'PAUSE':
-  //       uploadBytesResumable(photosRef, file!).pause();
-  //       console.log(' Blob | Uint8Array | ArrayBuffer');
-  //       break;
-  //     case 'RESUME':
-  //       uploadBytesResumable(photosRef, file!).resume();
-  //       console.log('File is resumed.');
-  //       break;
-  //     case 'CANCEL':
-  //       uploadBytesResumable(photosRef, file!).cancel();
-  //       console.log('File is canceled.');
-  //       break;
-  //     default:
-  //       console.error(`${state} match nothing`);
-  //   }
-  // };
 
   console.log(tag)
   return (
@@ -181,7 +154,7 @@ export const AddFiles = () => {
     {({ handleChange, handleBlur, handleSubmit, values, setFieldValue, errors }) => (
        <View style={styles.container}>
          <Text style={styles.title}>
-          Adding a file
+          {i18n.t('AnotherForm.fileTitle')}
           </Text>
       
          <Picker
@@ -193,71 +166,57 @@ export const AddFiles = () => {
               setTags(itemValue)
               setFieldValue('tags', itemValue)       
             }}>
-              {tagsArray.map((tags) => <Picker.Item 
-              label={tags.tag} 
-              value={tags.value} 
-              key={tags.tag} />)
+              {tagsArray.map((tags) => 
+              <Picker.Item 
+                label={tags.tag} 
+                value={tags.value} 
+                key={tags.tag}
+              />)
               }
           </Picker>
 
           { errors.tags && <Text style={styles.error}>{errors.tags}</Text> }
-
-           <Button title="Add a file" onPress={pickImage} />
       
+           <TouchableOpacity
+            style={styles.buttonFile}
+            // @ts-ignore
+            onPress={pickImage}
+            accessibilityLabel="button submit a file to storage"
+          >
+           <Text style={[styles.textButton, styles.textFile]}>
+             {i18n.t('AnotherForm.fileInput')}
+             </Text>
+         </TouchableOpacity>
 
          <TouchableOpacity
-         style={styles.button}
-         // @ts-ignore
-         onPress={handleSubmit}
-         accessibilityLabel="button submit a file to storage"
+          style={styles.button}
+          // @ts-ignore
+          onPress={handleSubmit}
+          accessibilityLabel="button submit a file to storage"
          >
-           <Text style={styles.textButton}>Send</Text>
+          <Text style={styles.textButton}>
+            {i18n.t('AnotherForm.send')}
+          </Text>
          </TouchableOpacity>
-       </View>
-     )}
-        
-        {/* <View 
-        // className={styles.form__field}
-        >
-          <Text style={styles.title}>
-            {/* {data?.AnotherForm?.file} */}
-            {/* Files */}
-          {/* </Text> */}
-          {/* <Field
-            name='file'
-            type='file'
-            accept='.jpg, .jpeg, .png, .webp, .avif, .webm, .mp4, .apng'
-            onChange={handleChange}
-            placeholder={data?.AnotherForm?.file}
-            // className={styles.input}
-            required={true}
+        {/* { 
+          progressUpload >= 1 && !(valuesFields === i18n.t('AnotherForm.uploadFile')) &&
+          <Progress
+            value={progressUpload}
+            colorScheme='green'
+            isAnimated
+            hasStripe
+            min={0}
+            max={100}
+            w={280}
+            bg='blue.400'
+            m='1.5rem auto'
+            size='md'
           />
-        </div> */}
-        {/* </View>} */}
-    
-        
-        {/* { progressUpload >= 1 && !(valuesFields ===`${data?.AnotherForm?.uploadFile}`) &&
-        <Progress
-          value={progressUpload}
-          colorScheme='green'
-          isAnimated
-          hasStripe
-          min={0}
-          max={100}
-          w={280}
-          bg='blue.400'
-          m='1.5rem auto'
-          size='md'
-        />
         } */}
   
-        {/* {valuesFields !== '' && <Alerts valueFields={valuesFields} />} */}
-        
-        {/*<div>*/}
-        {/*  <PauseCircleTwoTone className={styles.icons} onClick={() => managedUpload('PAUSE')} />*/}
-        {/*  <ReloadOutlined className={styles.icons} onClick={() => managedUpload('RESUME')} />*/}
-        {/*  <LoadingOutlined className={styles.icons} onClick={() => managedUpload('CANCEL')} />*/}
-        {/*</div>*/}
+        { valuesFields !== '' && <Alerts valueFields={valuesFields} /> }
+       </View>
+     )}
     </Formik>
   );
 };
@@ -274,41 +233,57 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 36,
     margin: 15,
-    justifyContent: 'center'
-  },
-  button: {
-    width: 100,
-    height: 40,
-    fontSize: 22,
-    margin: 10,
-    padding: 7,
-    alignItems: 'center',
-    backgroundColor: '#ddf',
-    borderRadius: 20
-  },
-  textButton: {
-    fontSize: 22,
-    alignSelf: 'center',
+    color: globalColors.$primary__color,
     justifyContent: 'center',
-    padding: 0,
-    color: '#333'
-  },
-  input: {
-    width: 260,
-    height: 40,
-    borderRadius: 20,
-    fontSize: 20,
-    backgroundColor: '#ddf',
-    margin: 20,
-    padding: 10
   },
   picker: {
-    width: '60%',
-    height: 40,
-    borderColor: '#ddf'
+    width: 250,
+    maxHeight: 100,
+    marginVertical: 15,
+    marginTop: 30,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    borderColor: globalColors.$secondary__color,
+    backgroundColor: globalColors.$third__color
   },
-  pickerItem: {
-    
+  button: {
+    width: 80,
+    height: 80,
+    margin: 27,
+    alignItems: 'center',
+    backgroundColor: globalColors.$another__color,
+    borderStyle: 'solid',
+    borderWidth: 2.5,
+    borderColor: globalColors.$another__color,
+    borderRadius: 80
+  },
+  textButton: {
+    width: '100%',
+    height: '100%',
+    fontSize: 22,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: globalColors.$dark__background__light__color,
+    textAlign: 'center',
+    paddingVertical: 25
+  },
+  buttonFile: {
+    width: 250,
+    height: 45,
+    margin: 20,
+    padding: 5,
+    borderWidth: .5,
+    borderColor: globalColors.$third__color,
+    backgroundColor: globalColors.$third__color
+  },
+  textFile: {
+    width: 200,
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    borderRadius: 1,
+    borderColor: globalColors.$third__color,
+    backgroundColor: globalColors.$third__color,
   },
   error: {
     fontSize: 25,
